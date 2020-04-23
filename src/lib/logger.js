43f8c4bdb5683173d2fc979afd743bf5d5a3a516
GonzaloCedillo <gonzalo.cedillo@sngular.team>
2020-04-24 00:09:13 +0200

    Test logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -2,17 +2,16 @@ const winston = require('winston');
 
 const today = new Date();
 const fileDate = `${today.getDate()}-${today.getMonth()}-${today.getFullYear()}`;
-const myFormat = winston.format.printf(({ message }) => {
-  return message;
-});
-const logger = winston.createLogger({
-  format: winston.format.combine(myFormat),
-  transports: [
-    new winston.transports.File({ filename: `logs/${fileDate}.log` }),
-  ],
-});
 
 function log(message) {
+  const myFormat = winston.format.printf(({ message }) => message);
+  const logger = winston.createLogger({
+    format: winston.format.combine(myFormat),
+    transports: [
+      new winston.transports.File({ filename: `logs/${fileDate}.log` }),
+    ],
+  });
+
   logger.log('info', message);
 }
 
