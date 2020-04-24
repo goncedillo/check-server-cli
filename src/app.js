d03453908d1fff3bd4603e61e9c962afd6041100
GonzaloCedillo <gonzalo.cedillo@sngular.team>
2020-04-24 21:37:01 +0200

    App tests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const path = require('path');
+const fs = require('fs');
 const childProcess = require('child_process');
 const dayjs = require('dayjs');
 const CronJob = require('cron').CronJob;
@@ -9,10 +10,9 @@ const logger = require('./lib/logger');
 const checker = require('./lib/checker');
 const weblogger = require('./lib/weblogger');
 
-const config = require('../config.json');
-const servers = [...config.servers];
+const configError = new Error('You must povide a valid config.json');
 
-async function checkServer(server) {
+async function checkServer(server, servers, info) {
   const filePath = path.resolve(process.cwd(), 'static', 'current-status.json');
   const now = dayjs().toString();
   const dateToRegister = dayjs(now).format('DD-MM-YYYY HH:mm');
@@ -26,34 +26,45 @@ async function checkServer(server) {
     date: now,
     status,
     filePath,
-    info: config.info,
+    info,
   });
 }
 
-function checkAllServers() {
+function checkAllServers(servers, configInfo) {
   servers.forEach((server) => {
-    checkServer(server);
+    checkServer(server, servers, configInfo);
   });
 }
 
-function checkForConfig() {
-  if (!config.servers || !config.servers.length) {
-    return console.log(`
-        ¡Debes proporcionar un config.json válido!
-    `);
-  }
+function loadConfig(pathToConfig) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(pathToConfig, (err, data) => {
+      if (err) reject(configError);
+
+      const config = JSON.parse(data);
+
+      if (!config.servers || !config.servers.length) {
+        return reject(configError);
+      }
+
+      resolve(config);
+    });
+  });
 }
 
-function start() {
-  checkForConfig();
+async function start() {
+  const config = await loadConfig(path.resolve(process.cwd(), 'config.json'));
 
-  new CronJob(`${config.timer || 5} * * * * *`, checkAllServers).start();
+  new CronJob(`${config.timer || 5} * * * *`, () => {
+    checkAllServers(config.servers, config.info);
+  }).start();
 
   childProcess.spawn('npx', ['serve'], {
     cwd: path.resolve(process.cwd(), 'static'),
     stdio: 'inherit',
   });
-  checkAllServers();
+
+  checkAllServers(config.servers, config.info);
 }
 
 module.exports = {
